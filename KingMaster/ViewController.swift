0b9d1967289b0a68c09567146f6f212c7f7df45b
Ricki Bin Yamin <ricki.yamin@dana.id>
2020-09-05 15:56:21 +0700

    Initial Commit

---
+++ KingMaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KingMaster
+//
+//  Created by Ricki Bin Yamin on 05/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
