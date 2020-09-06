0f7aa50e931c469e2e53f42ae44adc2ce5d53c65
Ricki Bin Yamin <ricki.yamin@dana.id>
2020-09-06 08:49:28 +0700

    add tableview

--- KingMaster/Screen/ScanAlbums/Views/ScanAlbumsView.swift
+++ KingMaster/Screen/ScanAlbums/Views/ScanAlbumsView.swift
@@ -11,4 +11,39 @@ import RBToolkit
 
 class ScanAlbumsView: View {
 	
+	// MARK: - Public Properties
+	
+	lazy var tableView: UITableView = {
+		let tableView = UITableView()
+		tableView.translatesAutoresizingMaskIntoConstraints = false
+		
+		return tableView
+	}()
+	
+	// MARK: - Life Cycle
+	
+	override func setViews() {
+		configureView()
+	}
+	
+	// MARK: - Public Method
+	
+	func reloadData() {
+		tableView.reloadData()
+	}
+	
+	// MARK: - Private Method
+	
+	private func configureView() {
+		backgroundColor = .systemBackground
+		addAllSubviews(views: [tableView])
+		
+		NSLayoutConstraint.activate([
+			tableView.topAnchor.constraint(equalTo: self.topAnchor),
+			tableView.leftAnchor.constraint(equalTo: self.leftAnchor),
+			tableView.rightAnchor.constraint(equalTo: self.rightAnchor),
+			tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+		])
+	}
+	
 }
