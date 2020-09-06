1a329d622d724324aafab4f76226dbf14f06ec1a
Ricki Bin Yamin <ricki.yamin@dana.id>
2020-09-06 14:34:34 +0700

    add delegate and datasource

--- KingMaster/Screen/ScanAlbums/Views/ScanAlbumsView.swift
+++ KingMaster/Screen/ScanAlbums/Views/ScanAlbumsView.swift
@@ -64,3 +64,15 @@ extension ScanAlbumsView {
 		
 	}
 }
+
+extension ScanAlbumsView: UITableViewDelegate, UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 0
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return UITableViewCell()
+	}
+	
+	
+}
