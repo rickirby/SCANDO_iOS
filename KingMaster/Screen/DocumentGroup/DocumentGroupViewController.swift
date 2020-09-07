4914776c8d8e6cba31d37b5731783eba815a8e77
Ricki Bin Yamin <ricki.yamin@dana.id>
2020-09-07 19:30:15 +0700

    setting controller for document group

--- KingMaster/Screen/DocumentGroup/DocumentGroupViewController.swift
+++ KingMaster/Screen/DocumentGroup/DocumentGroupViewController.swift
@@ -11,4 +11,28 @@ import RBToolkit
 
 class DocumentGroupViewController: ViewController<DocumentGroupView> {
 	
+	// MARK: - Life Cycle
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		configureLoadNavigationBar()
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		configureNavigationBar()
+	}
+	
+	// MARK: - Private Methods
+	
+	private func configureLoadNavigationBar() {
+		title = "Documents"
+	}
+	
+	private func configureNavigationBar() {
+		setLargeTitleDisplayMode(.never)
+		navigationController?.interactivePopGestureRecognizer?.isEnabled = true
+	}
 }
