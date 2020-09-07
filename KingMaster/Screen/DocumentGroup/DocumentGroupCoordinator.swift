28c6d43f64bff1d26c95f0a340c0715ff32ed4bf
Ricki Bin Yamin <ricki.yamin@dana.id>
2020-09-07 19:10:16 +0700

    document group coordinator

---
+++ KingMaster/Screen/DocumentGroup/DocumentGroupCoordinator.swift
@@ -0,0 +1,38 @@
+//
+//  DocumentGroupCoordinator.swift
+//  KingMaster
+//
+//  Created by Ricki Bin Yamin on 07/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+
+class DocumentGroupCoordinator: Coordinator {
+	
+	var rootViewController: UIViewController {
+		guard let navigationController = navigationController else {
+			return UIViewController()
+		}
+		
+		return navigationController
+	}
+	
+	private weak var navigationController: UINavigationController?
+	
+	init(navigationController: UINavigationController) {
+		self.navigationController = navigationController
+	}
+	
+	func start() {
+		let vc = makeDocumentGroupViewController()
+		
+		Router.shared.push(vc, on: self)
+	}
+	
+	private func makeDocumentGroupViewController() -> UIViewController {
+		let vc = DocumentGroupViewController()
+		
+		return vc
+	}
+}
