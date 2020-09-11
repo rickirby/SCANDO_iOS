e0eb2eb7f504168a99370c30f63b4019f7a19562
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-11 07:25:41 +0700

    adding scando constant

---
+++ KingMaster/Common/Constant/SCANDOConstant.swift
@@ -0,0 +1,13 @@
+//
+//  SCANDOConstant.swift
+//  KingMaster
+//
+//  Created by Ricki Private on 11/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+
+class SCANDOConstant {
+    
+}
