dd9de6d47bb83304a7826c1fa63738ad9cfe0bcb
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-11 07:37:25 +0700

    add alert view

---
+++ KingMaster/Common/Components/AlertView.swift
@@ -0,0 +1,13 @@
+//
+//  AlertView.swift
+//  KingMaster
+//
+//  Created by Ricki Private on 11/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+
+class AlertView {
+    
+}
