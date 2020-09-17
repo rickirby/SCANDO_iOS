9ee01b0d885ccc09486e974d33f2e5df3e8a26d1
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-17 19:39:30 +0700

    add view component

--- KingMaster/Screen/EditScan/Views/EditScanView.swift
+++ KingMaster/Screen/EditScan/Views/EditScanView.swift
@@ -8,7 +8,36 @@
 
 import UIKit
 import RBToolkit
+import RBCameraDocScan
 
 class EditScanView: View {
 	
+	var quadViewWidthConstraint = NSLayoutConstraint()
+	var quadViewHeightConstraint = NSLayoutConstraint()
+	
+	lazy var capturedImageView: UIImageView = {
+		let imageView = UIImageView()
+		imageView.translatesAutoresizingMaskIntoConstraints = false
+		imageView.clipsToBounds = true
+		imageView.isOpaque = true
+		imageView.backgroundColor = .systemBackground
+		imageView.contentMode = .scaleAspectFit
+		
+		return imageView
+	}()
+	
+	lazy var quadView: QuadrilateralView = {
+		let quadView = QuadrilateralView()
+		quadView.editable = true
+		quadView.translatesAutoresizingMaskIntoConstraints = false
+		return quadView
+	}()
+	
+	lazy var activityIndicator: UIActivityIndicatorView = {
+		let activityIndicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.large)
+		activityIndicator.color = .white
+		activityIndicator.hidesWhenStopped = true
+		activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+		return activityIndicator
+	}()
 }
