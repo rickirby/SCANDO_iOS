a3cc0afb224db4eb13dbed9cfb5631a409920e92
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-17 19:24:21 +0700

    add class

---
+++ KingMaster/Screen/EditScan/Views/EditScanView.swift
@@ -0,0 +1,14 @@
+//
+//  EditScanView.swift
+//  KingMaster
+//
+//  Created by Ricki Private on 17/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+import RBToolkit
+
+class EditScanView: View {
+	
+}
