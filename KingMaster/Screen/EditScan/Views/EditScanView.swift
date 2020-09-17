bbdae1066f8bf4724e7c2a02745fbe7018551d57
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-17 19:43:05 +0700

    configure view

--- KingMaster/Screen/EditScan/Views/EditScanView.swift
+++ KingMaster/Screen/EditScan/Views/EditScanView.swift
@@ -47,5 +47,32 @@ class EditScanView: View {
 	
 	override func setViews() {
 		super.setViews()
+		
+		configureView()
+	}
+	
+	// MARK: - Private Method
+	
+	private func configureView() {
+		backgroundColor = .systemBackground
+		addAllSubviews(views: [capturedImageView, quadView, activityIndicator])
+		
+		quadViewWidthConstraint = quadView.widthAnchor.constraint(equalToConstant: 0.0)
+		quadViewHeightConstraint = quadView.heightAnchor.constraint(equalToConstant: 0.0)
+		
+		NSLayoutConstraint.activate([
+			capturedImageView.topAnchor.constraint(equalTo: self.topAnchor),
+			capturedImageView.leftAnchor.constraint(equalTo: self.leftAnchor),
+			capturedImageView.rightAnchor.constraint(equalTo: self.rightAnchor),
+			capturedImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+			
+			quadView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+			quadView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+			quadViewWidthConstraint,
+			quadViewHeightConstraint,
+			
+			activityIndicator.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+			activityIndicator.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+		])
 	}
 }
