0d3addfcfef3f8adb2606e15d3fcd248ee309b12
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-19 06:06:09 +0700

    add models for passing data

---
+++ KingMaster/Screen/Preview/Models/PreviewData.swift
@@ -0,0 +1,18 @@
+//
+//  PreviewData.swift
+//  KingMaster
+//
+//  Created by Ricki Private on 19/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+import RBCameraDocScan
+
+extension PreviewCoordinator {
+	
+	struct PreviewData {
+		var image: UIImage
+		var quad: Quadrilateral
+	}
+}
