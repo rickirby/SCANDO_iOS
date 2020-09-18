c265b680d339fb690b838b7f6af3288f6993256b
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-18 21:28:54 +0700

    add preview class

---
+++ KingMaster/Screen/Preview/PreviewViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PreviewViewController.swift
+//  KingMaster
+//
+//  Created by Ricki Private on 18/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+import RBToolkit
+
+class PreviewViewController: ViewController<PreviewView> {
+	
+}
---
+++ KingMaster/Screen/Preview/Views/PreviewView.swift
@@ -0,0 +1,14 @@
+//
+//  PreviewView.swift
+//  KingMaster
+//
+//  Created by Ricki Private on 18/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+import RBToolkit
+
+class PreviewView: View {
+	
+}
