9c868c7dfcdf680ec154411d5874e483ae3c5adf
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-19 23:12:49 +0700

    add datamanager class

---
+++ KingMaster/Data/DataManager.swift
@@ -0,0 +1,14 @@
+//
+//  DataManager.swift
+//  KingMaster
+//
+//  Created by Ricki Private on 19/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class DataManager {
+	
+}
