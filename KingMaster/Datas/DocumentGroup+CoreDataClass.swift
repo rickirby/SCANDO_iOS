60b02ef25d88c20e519d2013a2e9becfb567fbdc
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-19 23:04:00 +0700

    safe from error

---
+++ KingMaster/Datas/DocumentGroup+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  DocumentGroup+CoreDataClass.swift
+//  KingMaster
+//
+//  Created by Ricki Private on 19/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(DocumentGroup)
+public class DocumentGroup: NSManagedObject {
+
+}
