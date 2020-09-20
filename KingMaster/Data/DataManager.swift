4f97117ff1b8f642d022d562119206ece74d9c4f
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-20 07:54:51 +0700

    add persistent container

--- KingMaster/Data/DataManager.swift
+++ KingMaster/Data/DataManager.swift
@@ -11,4 +11,20 @@ import CoreData
 
 class DataManager {
 	
+	// MARK: - Public Properties
+	
+	static let shared: DataManager = DataManager()
+	
+	// MARK: - Private Properties
+	
+	private lazy var persistentContainer: NSPersistentContainer = {
+		let container = NSPersistentContainer(name: "KingMaster")
+		container.loadPersistentStores { (_, error) in
+			if let error = error as NSError? {
+				fatalError("Unresolved error \(error), \(error.userInfo)")
+			}
+		}
+		
+		return container
+	}()
 }
