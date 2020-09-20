8946e2e4c16e24dbb24b88e5224a61f1b1da4e8d
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-20 10:06:16 +0700

    call reload data on viewwillappear

--- KingMaster/Screen/ScanAlbums/ScanAlbumsViewController.swift
+++ KingMaster/Screen/ScanAlbums/ScanAlbumsViewController.swift
@@ -41,8 +41,7 @@ class ScanAlbumsViewController: ViewController<ScanAlbumsView> {
 		super.viewWillAppear(animated)
 		
 		configureBar()
-		model.fetchData()
-//		screenView.tableViewData = dummyData
+		reloadData()
 	}
 	
 	// MARK: - Private Method
