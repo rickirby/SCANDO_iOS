8225ba715d04413f7e885346b32aee7fd60acc9f
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-20 08:39:48 +0700

    nsfetchedresultcontroller delegate conform

--- KingMaster/Screen/ScanAlbums/Models/ScanAlbumsModel.swift
+++ KingMaster/Screen/ScanAlbums/Models/ScanAlbumsModel.swift
@@ -9,11 +9,13 @@
 import UIKit
 import CoreData
 
-class ScanAlbumsModel {
+class ScanAlbumsModel: NSObject {
 	
 	// MARK: - Public Properties
 	
 	enum ModelEvent {
+		case beginUpdates
+		case endUpdates
 		case insertData
 		case deleteData
 		case updateData
@@ -21,4 +23,40 @@ class ScanAlbumsModel {
 	}
 	
 	var onModelEvent: ((ModelEvent) -> Void)?
+	
+	// MARK: - Private Properties
+	
+	lazy var fetchedResultsController: NSFetchedResultsController<DocumentGroup> = {
+		let managedContext = DataManager.shared.persistentContainer.viewContext
+		let fetchRequest = NSFetchRequest<DocumentGroup>(entityName: "DocumentGroup")
+		let sort = NSSortDescriptor(key: "date", ascending: false)
+		fetchRequest.sortDescriptors = [sort]
+		
+		let fetchedResultsController = NSFetchedResultsController<DocumentGroup>(fetchRequest: fetchRequest, managedObjectContext: managedContext, sectionNameKeyPath: nil, cacheName: nil)
+		
+		return fetchedResultsController
+	}()
+	
+	// MARK: - Life Cycle
+	
+	override init() {
+		super.init()
+		
+		fetchedResultsController.delegate = self
+	}
+}
+
+extension ScanAlbumsModel: NSFetchedResultsControllerDelegate {
+	
+	func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+		
+	}
+	
+	func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+		
+	}
+	
+	func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+		
+	}
 }
