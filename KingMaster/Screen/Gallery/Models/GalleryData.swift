56c6bf9ec897fc96c17f5736514b7f9185337c05
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-22 23:15:09 +0700

    add gallery data model

---
+++ KingMaster/Screen/Gallery/Models/GalleryData.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryData.swift
+//  KingMaster
+//
+//  Created by Ricki Private on 22/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+
+extension GalleryCoordinator {
+	
+	struct GalleryData {
+		let documentGroup: DocumentGroup
+		let selectedIndex: Int
+	}
+}
