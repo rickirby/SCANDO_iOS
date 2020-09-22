15de9b3a83ef6ee03265e8fed370284926c7c61b
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-22 22:26:00 +0700

    add model for picker

---
+++ KingMaster/Screen/ScanImagePicker/Models/ScanImagePickerData.swift
@@ -0,0 +1,16 @@
+//
+//  ScanImagePickerData.swift
+//  KingMaster
+//
+//  Created by Ricki Private on 22/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+
+extension ScanImagePickerCoordinator {
+	
+	struct ScanImagePickerData {
+		let documentGroup: DocumentGroup?
+	}
+}
