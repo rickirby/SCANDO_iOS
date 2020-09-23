0973f70c7d34d28fb137327bbf691c3da6ecebf0
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-23 07:45:36 +0700

    add cache class

---
+++ KingMaster/Screen/Gallery/Models/GalleryCache.swift
@@ -0,0 +1,31 @@
+//
+//  GalleryCache.swift
+//  KingMaster
+//
+//  Created by Ricki Private on 23/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+
+class GalleryCache {
+	
+	struct GalleryCacheModel {
+		let index: Int
+		var images: [UIImage]
+		var sortedDocuments: [Document]
+		var isImagesReady = false
+		var isSortedDocumentsReady = false
+	}
+	
+	static var cacheData: [GalleryCacheModel] = []
+	
+	static func getCache(for index: Int) -> GalleryCacheModel? {
+		
+		guard let cache = cacheData.first(where: {$0.index == index}) else {
+			return nil
+		}
+		
+		return cache
+	}
+}
