0ab7d4680ce077b110a5936bb0d7531464c4eaca
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-23 07:52:50 +0700

    add DocumentGroupData for passing data

---
+++ KingMaster/Screen/DocumentGroup/Models/DocumentGroupData.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentGroupData.swift
+//  KingMaster
+//
+//  Created by Ricki Private on 23/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+
+extension DocumentGroupCoordinator {
+	
+	struct DocumentGroupData {
+		let index: Int
+		let documentGroup: DocumentGroup
+	}
+}
