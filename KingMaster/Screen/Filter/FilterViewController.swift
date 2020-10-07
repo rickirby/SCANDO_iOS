69ff266bf24fe3ad1831a4d0b253dac1dd2d355a
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-10-07 08:43:26 +0700

    add filterview and viewcontroller

---
+++ KingMaster/Screen/Filter/FilterViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FilterViewController.swift
+//  Production
+//
+//  Created by Ricki Private on 07/10/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+import RBToolkit
+
+class FilterViewController: ViewController<FilterView> {
+	
+}
---
+++ KingMaster/Screen/Filter/Views/FilterView.swift
@@ -0,0 +1,14 @@
+//
+//  FilterView.swift
+//  Production
+//
+//  Created by Ricki Private on 07/10/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+import RBToolkit
+
+class FilterView: View {
+	
+}
