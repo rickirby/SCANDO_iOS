cd53462de29dba26551b736936dcac33d7372e14
Ricki Bin Yamin <ricki.yamin@dana.id>
2020-10-09 08:04:49 +0700

    add filterusersetting class

---
+++ KingMaster/Screen/Filter/Models/FilterUserSetting.swift
@@ -0,0 +1,13 @@
+//
+//  FilterUserSetting.swift
+//  KingMaster
+//
+//  Created by Ricki Bin Yamin on 09/10/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+
+class FilterUserSettting: UserSetting {
+	
+}
