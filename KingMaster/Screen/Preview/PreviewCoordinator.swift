9eefa39156e5793ed9478b893cdf7fbfe1a2cf92
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-10-11 17:30:54 +0700

    remove reference of filtercoordinator on previewcoordinator

--- KingMaster/Screen/Preview/PreviewCoordinator.swift
+++ KingMaster/Screen/Preview/PreviewCoordinator.swift
@@ -22,7 +22,6 @@ class PreviewCoordinator: Coordinator {
 	var passedData: (() -> PreviewData)?
 	
 	private weak var navigationController: UINavigationController?
-	private var filterCoordinator: FilterCoordinator?
 	
 	init(navigationController: UINavigationController) {
 		self.navigationController = navigationController
@@ -62,13 +61,4 @@ class PreviewCoordinator: Coordinator {
 		return vc
 	}
 	
-	private func openFilter(processedImage: UIImage) {
-		filterCoordinator = nil
-		filterCoordinator = FilterCoordinator(navigationController: self.rootViewController as? UINavigationController ?? UINavigationController())
-		filterCoordinator?.passedData = {
-			return FilterCoordinator.FilterData(image: processedImage)
-		}
-		
-		filterCoordinator?.start()
-	}
 }
