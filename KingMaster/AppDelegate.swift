afeff14b3f6e40e19849c83735aaa2ac8594db95
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-10-13 08:03:13 +0700

    change to cloudkit container

--- KingMaster/AppDelegate.swift
+++ KingMaster/AppDelegate.swift
@@ -35,14 +35,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	
 	// MARK: - Core Data stack
 	
-	lazy var persistentContainer: NSPersistentContainer = {
+	lazy var persistentContainer: NSPersistentCloudKitContainer = {
 		/*
 		The persistent container for the application. This implementation
 		creates and returns a container, having loaded the store for the
 		application to it. This property is optional since there are legitimate
 		error conditions that could cause the creation of the store to fail.
 		*/
-		let container = NSPersistentContainer(name: "KingMaster")
+		let container = NSPersistentCloudKitContainer(name: "KingMaster")
 		container.loadPersistentStores(completionHandler: { (storeDescription, error) in
 			if let error = error as NSError? {
 				// Replace this implementation with code to handle the error appropriately.
