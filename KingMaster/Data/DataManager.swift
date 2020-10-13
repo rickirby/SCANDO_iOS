b09f863ead3c1e90f1ed0372bb64f4c12c54e8db
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-10-13 17:58:27 +0700

    add more property to persistent container

--- KingMaster/Data/DataManager.swift
+++ KingMaster/Data/DataManager.swift
@@ -17,23 +17,41 @@ class DataManager {
 	
 	var viewContext: NSManagedObjectContext {
 		let viewContext = persistentContainer.viewContext
-		viewContext.automaticallyMergesChangesFromParent = true
+		
 		return viewContext
 	}
 	
 	// MARK: - Private Properties
 	
 	private lazy var persistentContainer: NSPersistentCloudKitContainer = {
+		
 		let container = NSPersistentCloudKitContainer(name: "KingMaster")
+		
+		guard let description = container.persistentStoreDescriptions.first else {
+            fatalError("###\(#function): Failed to retrieve a persistent store description.")
+        }
+        description.setOption(true as NSNumber, forKey: NSPersistentHistoryTrackingKey)
+        description.setOption(true as NSNumber, forKey: NSPersistentStoreRemoteChangeNotificationPostOptionKey)
+		
 		container.loadPersistentStores { (_, error) in
 			if let error = error as NSError? {
 				fatalError("Unresolved error \(error), \(error.userInfo)")
 			}
 		}
 		
+		container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        container.viewContext.transactionAuthor = "app"
+		
+		container.viewContext.automaticallyMergesChangesFromParent = true
+        do {
+            try container.viewContext.setQueryGenerationFrom(.current)
+        } catch {
+            fatalError("###\(#function): Failed to pin viewContext to the current generation:\(error)")
+        }
+		
 		return container
 	}()
-	
+
 	// MARK: - Public Method
 	
 	func saveContext() {
