e21ec40539ae1e73f779be00b8991b90d2d926eb
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-11-07 23:55:14 +0700

    finish setting model

--- KingMaster/Screen/SettingScreen/Models/SettingModel.swift
+++ KingMaster/Screen/SettingScreen/Models/SettingModel.swift
@@ -10,4 +10,25 @@ import UIKit
 
 class SettingModel {
 	
+	struct SettingTableData {
+		let sectionTitle: String
+		let rowsData: [SettingTableRowData]
+	}
+	
+	struct SettingTableRowData {
+		let title: String
+		let navigationEvent: SettingViewController.NavigationEvent
+	}
+	
+	func getTableData() -> [SettingTableData] {
+		return [
+			SettingTableData(sectionTitle: "General", rowsData: [
+				SettingTableRowData(title: "About", navigationEvent: .didTapAbout),
+				SettingTableRowData(title: "Help", navigationEvent: .didTapHelp)
+			]),
+			SettingTableData(sectionTitle: "Connection", rowsData: [
+				SettingTableRowData(title: "Connection Setting", navigationEvent: .didTapConnectionSetting)
+			])
+		]
+	}
 }
