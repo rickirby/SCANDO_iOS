5d568f00cb8e20793fc41e45b4c53486306bd044
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-11-08 00:27:29 +0700

    configure vc

--- KingMaster/Screen/SettingScreen/SettingViewController.swift
+++ KingMaster/Screen/SettingScreen/SettingViewController.swift
@@ -21,5 +21,48 @@ class SettingViewController: ViewController<SettingView> {
 	
 	var onNavigationEvent: ((NavigationEvent) -> ())?
 	
+	// MARK: - Private Properties
+	
+	let model = SettingModel()
+	var tableData: [SettingModel.SettingTableData] = []
+	
 	// MARK: - Life Cycles
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		configureModel()
+		configureViewEvent()
+		configureViewData()
+	}
+	
+	// MARK: - Private Methods
+	
+	private func configureModel() {
+		tableData = model.getTableData()
+	}
+	
+	private func configureViewEvent() {
+		screenView.onViewEvent = { [weak self] (viewEvent: SettingView.ViewEvent) in
+			switch viewEvent {
+			case .didSelectRow(indexPath: let indexPath):
+				guard indexPath.section < self?.tableData.count ?? 0,
+					let rowData = self?.tableData[indexPath.section].rowsData,
+					indexPath.row < rowData.count else {
+					return
+				}
+				
+				self?.onNavigationEvent?(rowData[indexPath.row].navigationEvent)
+			}
+		}
+	}
+	
+	private func configureViewData() {
+		screenView.viewDataSupply = {
+			let viewData: [[String]] = self.tableData.map { $0.rowsData.map { $0.title } }
+			
+			return viewData
+		}
+	}
+	
 }
