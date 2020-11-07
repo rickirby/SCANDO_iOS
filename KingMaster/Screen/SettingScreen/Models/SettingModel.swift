84394fbb06865f6e3c5d639dbd5c83c5792b0ca7
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-11-07 23:44:43 +0700

    add settingmodel class

---
+++ KingMaster/Screen/SettingScreen/Models/SettingModel.swift
@@ -0,0 +1,13 @@
+//
+//  SettingModel.swift
+//  KingMaster
+//
+//  Created by Ricki Private on 07/11/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+
+class SettingModel {
+	
+}
