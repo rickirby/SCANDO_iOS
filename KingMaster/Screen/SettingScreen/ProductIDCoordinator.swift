4549829492f2fac6d1e0324e553a0b55827898d4
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-11-15 09:52:31 +0700

    add product id coordinator

---
+++ KingMaster/Screen/SettingScreen/ProductIDCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  ProductIDCoordinator.swift
+//  KingMaster
+//
+//  Created by Ricki Private on 15/11/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import Foundation
