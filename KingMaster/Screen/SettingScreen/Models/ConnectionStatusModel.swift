e159d3dc36ff89aaa41434b2004400d8160f2fb5
Ricki Bin Yamin <ricki.bin.yamin@gmail.com>
2020-12-01 22:50:44 +0700

    add connection status model

---
+++ KingMaster/Screen/SettingScreen/Models/ConnectionStatusModel.swift
@@ -0,0 +1,75 @@
+//
+//  ConnectionStatusModel.swift
+//  KingMaster
+//
+//  Created by Ricki Bin Yamin on 01/12/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import Foundation
+import SystemConfiguration.CaptiveNetwork
+import NetworkExtension
+import CoreLocation
+
+class ConnectionStatusModel {
+	
+	static let shared = ConnectionStatusModel()
+	
+	func checkConnectionStatus(onStartLoading: (() -> Void)? = nil, onStopLoading: (() -> Void)? = nil, onGotStatus: ((ConnectionStatusViewController.Status) -> Void)? = nil) {
+		guard let sharedSSID = ConnectionUserSetting.shared.read() else {
+			onGotStatus?(.disconnected)
+			return
+		}
+		
+		onStartLoading?()
+		
+		if sharedSSID == "" {
+			NetworkRequest.get(url: "http://192.168.4.1/checkresponse") { result in
+				ThreadManager.executeOnMain {
+					if let message = result["msg"] as? String, message == "OK" {
+						onGotStatus?(.directConnected)
+					} else {
+						onGotStatus?(.disconnected)
+					}
+					
+					onStopLoading?()
+				}
+			}
+			
+		} else {
+			if currentSSIDs().first == sharedSSID {
+				NetworkRequest.get(url: "http://scandohardware.local/checkresponse") { result in
+					ThreadManager.executeOnMain {
+						if let message = result["msg"] as? String, message == "OK" {
+							onGotStatus?(.sharedConnected)
+						} else {
+							onGotStatus?(.disconnected)
+						}
+						
+						onStopLoading?()
+					}
+				}
+			} else {
+				onGotStatus?(.differentNetwork)
+			}
+		}
+	}
+	
+	private func currentSSIDs() -> [String] {
+		
+		guard let interfaceNames = CNCopySupportedInterfaces() as? [String] else {
+			return []
+		}
+		
+		return interfaceNames.compactMap { name in
+			guard let info = CNCopyCurrentNetworkInfo(name as CFString) as? [String:AnyObject] else {
+				return nil
+			}
+			guard let ssid = info[kCNNetworkInfoKeySSID as String] as? String else {
+				return nil
+			}
+			return ssid
+		}
+	}
+	
+}
