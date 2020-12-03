39926d9fab52ad12bb482fa756e4957b63df02cb
Ricki Bin Yamin <ricki.bin.yamin@gmail.com>
2020-12-03 19:46:12 +0700

    add components on translation view

--- KingMaster/Screen/Translation/Views/TranslationView.swift
+++ KingMaster/Screen/Translation/Views/TranslationView.swift
@@ -11,4 +11,36 @@ import RBToolkit
 
 class TranslationView: View {
 	
+	// MARK: - Private Properties
+	
+	lazy var titleLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.font = .preferredFont(forTextStyle: .body)
+		label.textColor = .label
+		label.text = "Translation Result"
+		
+		return label
+	}()
+	
+	lazy var resultTextView: UITextView = {
+		let textView = UITextView()
+		textView.translatesAutoresizingMaskIntoConstraints = false
+		textView.layer.borderWidth = 1.0
+		textView.layer.borderColor = UIColor.gray.cgColor
+		textView.layer.cornerRadius = 3.0
+		
+		return textView
+	}()
+	
+	lazy var activityIndicator: UIActivityIndicatorView = {
+		let activityIndicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.large)
+		activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+		activityIndicator.color = .gray
+		activityIndicator.hidesWhenStopped = true
+		
+		return activityIndicator
+	}()
+	
+	
 }
