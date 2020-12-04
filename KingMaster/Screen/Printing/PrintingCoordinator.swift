15a69b2093b3fe331d0a47d9efb89bb9aa721215
Ricki Bin Yamin <ricki.bin.yamin@gmail.com>
2020-12-04 22:11:59 +0700

    fill base printing coordinator

--- KingMaster/Screen/Printing/PrintingCoordinator.swift
+++ KingMaster/Screen/Printing/PrintingCoordinator.swift
@@ -6,4 +6,41 @@
 //  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class PrintingCoordinator: Coordinator {
+	
+	// MARK: - Public Properties
+	
+	var rootViewController: UIViewController {
+		guard let navigationController = navigationController else {
+			return UIViewController()
+		}
+		
+		return navigationController
+	}
+	
+	// MARK: - Private Properties
+	
+	private weak var navigationController: UINavigationController?
+	
+	init(navigationController: UINavigationController) {
+		self.navigationController = navigationController
+	}
+	
+	// MARK: - Public Methods
+	
+	func start() {
+		let vc = makePrintingViewController()
+		
+		Router.shared.push(vc, on: self)
+	}
+	
+	// MARK: - Private Methods
+	
+	private func makePrintingViewController() -> UIViewController {
+		let vc = PrintingViewController()
+		
+		return vc
+	}
+}
