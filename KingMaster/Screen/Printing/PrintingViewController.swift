aab2c9ff1423aee9fca1e18faaf846297dc82e75
Ricki Bin Yamin <ricki.bin.yamin@gmail.com>
2020-12-04 23:25:41 +0700

    configuring navbar

--- KingMaster/Screen/Printing/PrintingViewController.swift
+++ KingMaster/Screen/Printing/PrintingViewController.swift
@@ -11,4 +11,23 @@ import RBToolkit
 
 class PrintingViewController: ViewController<PrintingView> {
 	
+	// MARK: - Life Cycles
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		configureBar()
+	}
+	
+	// MARK: - Private Methods
+	
+	private func configureBar() {
+		navigationController?.setNavigationBarHidden(true, animated: true)
+		navigationController?.setToolbarHidden(true, animated: true)
+		navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+	}
 }
